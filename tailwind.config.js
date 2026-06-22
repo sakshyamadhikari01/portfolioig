11a84190f9a4d08b53811ba8bf90f3ace566206a
sakshyamadhikari01 <sakshyamadhikari01@gmail.com>
2026-06-22 19:13:57 +0545

    made-some-files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+GPUShaderModule.exports = {
+    content:['./*.html'],
+    theme:{},
+      extend:{
+        "sm": "480px"
+      },
+},
+    fontfamily:{
+        Playfair Display:['Playfair Display', 'serif']
+    }
+plugins:[],
+}
